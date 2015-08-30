05c2a2cfdea6748211a9a4aa13306c6a3106160b
Robert Pohl <robert@sugarcubesolutions.com>
2015-08-30 20:16:11 +0200

    first

---
+++ lang/languages.js
@@ -0,0 +1,24 @@
+window.__mulang = {
+  en: {
+    pay_headline:'Pay amount {0} {1}',
+    intro:'Intro',
+    card_holder:'Card holder name',
+    card_number:'Card number',
+    card_type:'Card type',
+    cvc:'CVV code',
+    expiry:'Expiry date',
+    pay_button:'Finish payment',
+    safe_text:'Pay safely with 256 bit SSL encryption. No card numbers will be stored.'
+  },
+  sv: {
+    pay_headline:'Betala belopp {0} {1}',
+    intro:'Fyll i din betalinformation så kommer prenumerationen att komma igång!',
+    card_holder:'Korthavarens namn',
+    card_number:'Kortnummer',
+    card_type:'Korttyp',
+    cvc:'CVV-kod (kontrollsiffror):',
+    expiry:'Gilltighetstid:',
+    pay_button:'Betala',
+    safe_text:'Betala säkert med 256 bitars SSL kryptering. Inga kortupgifter kommer att sparas.'
+  }
+}
