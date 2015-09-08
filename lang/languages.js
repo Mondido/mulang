260d29bd1b5b8ab0e09e6ab93660327c1cf9b419
Robert Pohl <robert@sugarcubesolutions.com>
2015-09-08 16:06:12 +0200

    added de da

--- lang/languages.js
+++ lang/languages.js
@@ -1,24 +1,68 @@
 window.__mulang = {
   en: {
-    pay_headline:'Pay amount {0} {1}',
+    pay_headline:'Payment',
+    amount: 'Amount: <strong>{0} {1}</strong>',
     intro:'Intro',
     card_holder:'Card holder name',
+    card_holder_placeholder: 'Firstname Lastname',
     card_number:'Card number',
+    card_number_placeholder:'Your card number',
     card_type:'Card type',
+    card_type_placeholder: 'VISA / MASTERCARD',
     cvc:'CVV code',
     expiry:'Expiry date',
     pay_button:'Finish payment',
-    safe_text:'Pay safely with 256 bit SSL encryption. No card numbers will be stored.'
+    safe_text:'Pay safely with 256 bit SSL encryption. No card numbers will be stored.',
+    loading: 'Loading'
   },
   sv: {
-    pay_headline:'Betala belopp {0} {1}',
+    pay_headline:'Betalning',
+    amount: 'Summa: <strong>{0} {1}</strong>',
     intro:'Fyll i din betalinformation så kommer prenumerationen att komma igång!',
     card_holder:'Korthavarens namn',
+    card_holder_placeholder: 'Förnamn Efternamn',
     card_number:'Kortnummer',
+    card_number_placeholder:'Ditt kortnummer',
     card_type:'Korttyp',
+    card_type_placeholder: 'VISA / MASTERCARD',
     cvc:'CVV-kod (kontrollsiffror):',
     expiry:'Gilltighetstid:',
     pay_button:'Betala',
-    safe_text:'Betala säkert med 256 bitars SSL kryptering. Inga kortupgifter kommer att sparas.'
+    safe_text:'Betala säkert med 256 bitars SSL kryptering. Inga kortupgifter kommer att sparas.',
+    loading: 'Laddar'
+  },
+  de: {
+    pay_headline:'Bezahlung',
+    amount: 'Gesamt: <strong>{0} {1}</strong>',
+    intro:'Füllen Sie Ihre Zahlungsinformationen , und die Zeichnung , um loszulegen!',
+    card_holder:'Name des Karteninhabers',
+    card_holder_placeholder: 'Vorname Nachname',
+    card_number:'Kartennummern',
+    card_number_placeholder:'Ihre Kartennummer',
+    card_type:'Karten name',
+    card_type_placeholder: 'VISA / MASTERCARD',
+    cvc:'CVV-code (Prüfziffern):',
+    expiry:'Verfallsdatum:',
+    pay_button:'Zahlen',
+    safe_text:'Sicher zahlen mit 256 bit SSL verschlüsselung. Keine Karten werden upgifter gespeichert werden.',
+    loading: 'Loading'
+  },
+  da: {
+    pay_headline:'Betaling',
+    amount: 'Beløb: <strong>{0} {1}</strong>',
+    intro:'Intro!',
+    card_holder:'Kortindehaverens navn',
+    card_holder_placeholder: 'Fornavn Efternavn',
+    card_number:'kortnummer',
+    card_number_placeholder:'Dit kortnummer',
+    card_type:'Korttype',
+    card_type_placeholder: 'VISA / MASTERCARD',
+    cvc:'CVV-kode (kontrolcifre):',
+    expiry:'Udløbsdato:',
+    pay_button:'betale',
+    safe_text:'Betal sikkert med 256 bit SSL kryptering. Ingen kort bliver gemt upgifter.',
+    loading: 'Lastning'
   }
+
+
 }
